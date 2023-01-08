c551eca602a8a99a5a906ecc7103c3a75879af13
Daisuke Sato <dsato2@nait.ca>
2023-01-08 15:49:25 -0700

    added css

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">Daisuke</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
