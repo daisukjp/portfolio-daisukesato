29fbb55c2dc44a727157729424af91525bb2d9e7
Daisuke Sato <dsato2@nait.ca>
2023-01-09 06:28:51 -0700

    added header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,46 @@ const Header = () => {
         <header className="header">
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">Daisuke</a>
+
+                <div className='nav__menu'>
+                    <ul className='nav__list grid'>
+                        <li className='nav__item'>
+                            <a href="#home" className='nav__link'>
+                                <i className='uil uil-estate nav__icon'>Home</i>
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href="#about" className='nav__link'>
+                                <i className='uil uil-user nav__icon'>About</i>
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href="#skills" className='nav__link'>
+                                <i className='uil uil-file-alt nav__icon'>Skills</i>
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href="#projects" className='nav__link'>
+                                <i className='uil uil-briefcase-alt nav__icon'>Projects</i>
+                            </a>
+                        </li>
+
+                        <li className='nav__item'>
+                            <a href="#contact" className='nav__link'>
+                                <i className='uil uil-message nav__icon'>Contact</i>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <i className="uil uil-times nav__close"></i>
+                </div>
+
+                <div className='nav__toggle'>
+                <i className="uil uil-apps"></i>
+                </div>
             </nav>
         </header>
     )
