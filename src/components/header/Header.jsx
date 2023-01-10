789b331cc10b27c4bcf28d1c3c2a234bcb530195
Daisuke Sato <dsato2@nait.ca>
2023-01-09 18:47:55 -0700

    added header and main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,49 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./header.css";
 
 const Header = () => {
+    /* Toggle Menu */
+        const[Toggle, ShowMenu] = useState(false);
+
+
     return (
         <header className="header">
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">&lt;/DS&gt;</a>
 
-                <div className='nav__menu'>
+                <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className='nav__list grid'>
                         <li className='nav__item'>
-                            <a href="#home" className='nav__link'>
-                                <i className='uil uil-estate nav__icon'>Home</i>
+                            <a href="#home" className='nav__link active-link'>
+                                <i className='uil uil-estate nav__icon'></i>
+                                Home
                             </a>
                         </li>
 
                         <li className='nav__item'>
                             <a href="#about" className='nav__link'>
-                                <i className='uil uil-user nav__icon'>About</i>
+                                <i className='uil uil-user nav__icon'></i>
+                                About
                             </a>
                         </li>
 
                         <li className='nav__item'>
                             <a href="#skills" className='nav__link'>
-                                <i className='uil uil-file-alt nav__icon'>Skills</i>
+                                <i className='uil uil-file-alt nav__icon'></i>
+                                Skills
                             </a>
                         </li>
 
                         <li className='nav__item'>
                             <a href="#projects" className='nav__link'>
-                                <i className='uil uil-briefcase-alt nav__icon'>Projects</i>
+                                <i className='uil uil-briefcase-alt nav__icon'></i>
+                                Projects
                             </a>
                         </li>
 
                         <li className='nav__item'>
                             <a href="#contact" className='nav__link'>
-                                <i className='uil uil-message nav__icon'>Contact</i>
+                                <i className='uil uil-message nav__icon'></i>
+                                Contact
                             </a>
                         </li>
                     </ul>
 
-                    <i className="uil uil-times nav__close"></i>
+                    <i className="uil uil-times nav__close" onClick={() => ShowMenu(!Toggle)}></i>
                 </div>
 
-                <div className='nav__toggle'>
+                <div className='nav__toggle' onClick={() => ShowMenu(!Toggle)}>
                 <i className="uil uil-apps"></i>
                 </div>
             </nav>
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/film_jp/" className="home__social-icon" target="_blank">
+          <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://github.com/daisukjp" className="home__social-icon" target="_blank">
+          <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/daisuke-sato-2b82241a0/" className="home__social-icon" target="_blank">
+          <i class="uil uil-linkedin-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
