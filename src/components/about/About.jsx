3eaae05c89c6d7847129d6ed54bc1554ab8f9360
Daisuke Sato <dsato2@nait.ca>
2023-02-18 16:00:21 -0700

    completed qualification part

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 
                 <a download="" href={RS} className="button button__flex">Download Resume
                     <svg
-                    class="button__icon"
+                    className="button__icon"
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
                     height="24"
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,15 +4,15 @@ const Social = () => {
   return (
     <div className="home__social">
         <a href="https://www.instagram.com/film_jp/" className="home__social-icon" target="_blank">
-          <i class="uil uil-instagram"></i>
+          <i className="uil uil-instagram"></i>
         </a>
 
         <a href="https://github.com/daisukjp" className="home__social-icon" target="_blank">
-          <i class="uil uil-github-alt"></i>
+          <i className="uil uil-github-alt"></i>
         </a>
 
         <a href="https://www.linkedin.com/in/daisuke-sato-2b82241a0/" className="home__social-icon" target="_blank">
-          <i class="uil uil-linkedin-alt"></i>
+          <i className="uil uil-linkedin-alt"></i>
         </a>
     </div>
   )
