005a077e522a5b9a84ae6e5a53a39df600069b10
Daisuke Sato <dsato2@nait.ca>
2023-02-19 14:30:26 -0700

    contact section completed

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./contact.css"
+import Email from './Email'
+import Phone from './Phone'
+
+const Contact = () => {
+return (
+    <section className="contact section" id="contact">
+        <h2 className="section__title">Get in touch</h2>
+        <h3 className="section__subtitle">Contact me</h3>
+        <h3 className="contact__title">Talk to me</h3>
+
+        <div className="contact__container container grid">
+            <Email />
+            <Phone />
+        </div>
+    </section>
+)
+}
+
+export default Contact
