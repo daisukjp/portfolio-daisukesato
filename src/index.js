eb49b9194bebb5cc4fe4c2aeed5a3ae010c9b3f1
Daisuke Sato <dsato2@nait.ca>
2023-04-23 02:14:43 -0600

    added change

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import { ProjectsPage } from './components/Projects/ProjectsPage';
 import { Portfolio } from './components/ProjectList/Portfolio';
 import { Ethical } from './components/ProjectList/Ethical';
 import { Fuwa } from './components/ProjectList/Fuwa';
-import { Error } from './components/Error';
+// import { Error } from './components/Error';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ScrollToTop />
     <Routes>
-      <Route path="/" element={<App />} />
+      {/* <Route path="/" element={<App />} /> */}
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="projects/portfolio" element={<Portfolio />} />
       <Route path="projects/ethical" element={<Ethical />} />
       <Route path="projects/fuwafuwa" element={<Fuwa />} />
-      <Route path="*" element={<Error />} />
+      {/* <Route path="*" element={<Error />} /> */}
     </Routes>
   </BrowserRouter>
 );
