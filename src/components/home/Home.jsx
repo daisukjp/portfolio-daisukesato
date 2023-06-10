9c223f5be2af244fb3d772d5b1033f1bd086b173
Daisuke Sato <dsato2@nait.ca>
2023-06-10 12:31:49 -0600

    profile info updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -43,14 +43,20 @@ const Home = () => {
                 Hi, I'm Daisuke Sato, a software engineer who loves building cool things with code. In addition to coding, I also take photography, focusing on pet, portrait, and landscape photography.
             </p>
             </div>
-            <ul className="home__u animated-list">
+            <ul className="home__ul animated-list">
               <li className="home__li transition-opacity">
-                <a href="">
+                <a className="home__a" href="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="home__icon">
+                  <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"/>
+                </svg>
                   <span>Email me</span>
                 </a>
               </li>
               <li className="home__li transition-opacity">
-                <a href="">
+                <a className="home__a" href="">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="home__icon">
+                    <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"/>
+                  </svg>
                   <span>Connect with me</span>
                 </a>
               </li>
