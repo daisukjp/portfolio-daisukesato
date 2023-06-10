5125559d5e99949c6d5973f539348817a84d61aa
Daisuke Sato <dsato2@nait.ca>
2023-06-09 18:33:03 -0600

    have stared to make a bettr layout

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -19,24 +19,24 @@ const Data = () => {
     return (
         <div className="home__data">
             <Reveal keyframes={fadeInUp} delay={300} duration={600} triggerOnce>
-                <h1 className="name__intro">HELLO. I'M</h1>
+                {/* <h1 className="name__intro">HELLO. I'M</h1> */}
             </Reveal>
-            <Reveal className='onStep' keyframes={fadeInUp} delay={600} duration={600} triggerOnce>
+            {/* <Reveal className='onStep' keyframes={fadeInUp} delay={600} duration={600} triggerOnce>
                 <h1 className="home__title">
                     Daisuke
                 </h1>
                 <h1 className="name__bottom">
                 Sato
                 </h1>
-            </Reveal>
-            <Reveal className='onStep' keyframes={fadeInUp} delay={800} duration={900} triggerOnce>
-                <h3 className="home__subtitle">SOFTWARE DEVELOPER BASED IN Edmonton</h3>
+            </Reveal> */}
+            {/* <Reveal className='onStep' keyframes={fadeInUp} delay={800} duration={900} triggerOnce>
+                <h3 className="home__subtitle">Software Developer based in Edmonton</h3>
             
 
             <a href="#contact" className="button button__flex">
                 GET IN TOUCH
             </a>
-            </Reveal>
+            </Reveal> */}
 
         </div>
     )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 import Data from './Data';
 import { Reveal } from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
+import Avatar from "../../assets/about.jpg"
 
 const Home = () => {
 
@@ -18,11 +19,33 @@ const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
-          
-        <div className="home__content grid">
+          <div className="home__section-container">
+            <div className="home__name-intro">
+              <h1>Daisuke Sato</h1>
+              <p>I write code and take photos</p>
+            </div>
+
+            <div className="home__profile-pic">
+              <img src={Avatar} alt="" />
+            </div>
+
+            <div className="">
+
+            </div>
+
+            <p>
+              Hi, I'm Daisuke Sato, a software engineer who loves building cool things with code. In addition to coding, I also make YouTube videos, where I focus on tech, creative vlogs, and personal development.
+            </p>
+            <u>
+
+            </u>
+          </div>
+        {/* <div className="home__content grid">
           <Data />
-        <Reveal className='onStep' keyframes={fadeIn} delay={900} duration={1500} triggerOnce><div className="home__img"></div></Reveal>
-        </div>
+        <Reveal className='onStep' keyframes={fadeIn} delay={900} duration={1500} triggerOnce>
+          <div className="home__img"></div>
+        </Reveal>
+        </div> */}
       </div>
     </section>
   )
