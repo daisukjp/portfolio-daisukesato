6bb607ef88db5e849c135c7d0cd55c2fa7191c7d
Daisuke Sato <daisukjp1@gmail.com>
2023-08-23 23:56:07 -0600

    added project page

--- src/components/Projects/ProjectsPage.jsx
+++ src/components/Projects/ProjectsPage.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import { useState } from 'react'
-import "./projectsPage.css";
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
-import ScrollUp from '../scrollup/ScrollUp';
-import { ProjectsCard } from './ProjectsCard';
-import { useNavigate } from 'react-router-dom'
-import ProjectData from '../../ProjectData.json'
+import React from "react";
+import { useState } from "react";
+// import "./projectsPage.css";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import ScrollUp from "../scrollup/ScrollUp";
+import { ProjectsCard } from "./ProjectsCard";
+import { ProjectMainCard } from "./ProjectMainCard";
+// import { useNavigate } from 'react-router-dom'
+import { Reveal } from "react-awesome-reveal";
+import ProjectData from "../../ProjectData.json";
 
 export const ProjectsPage = () => {
-    const [datas, setDatas] = useState(ProjectData.projects)
+  const [datas, setDatas] = useState(ProjectData.projects);
 
-    const directionArr = ['row', 'row-reverse'];
-    const chooseDirection = (i) => {
-        if (i % 2 === 0) {
-            return directionArr[0];
-        } else {
-            return directionArr[1];
-        }
-    };
+  const directionArr = ["row", "row-reverse"];
+  const chooseDirection = (i) => {
+    if (i % 2 === 0) {
+      return directionArr[0];
+    } else {
+      return directionArr[1];
+    }
+  };
 
-    const width = window.innerWidth
-    const navigate = useNavigate()
-        return (
-        <>
-            <Header />
-                <h1 className="projectPage__projectTitle">
+  return (
+    <>
+      <Header />
+      {/* <h1 className="projectPage__projectTitle">
                         Projects
-                </h1>
-                <div className="projectsPage__container-main">
+                </h1> */}
+      {/* <div className="projectsPage__container-main">
                     {datas.map((data, i) => {
                         const direction = chooseDirection(i)
                         return <ProjectsCard key={data.title}  data={data} direction={direction} />
                     })}
-                </div>
-            <Footer />
-            <ScrollUp />
-    
-        </>
-        )
+                </div> */}
+      <div style={{ marginTop: "8rem" }}>
+        {/* <Reveal className="onStep" delay={600} duration={600} triggerOnce> */}
+          <div style={{}}>
+            <h2>Projects</h2>
+            <ul>
+              {datas.map((data, i) => {
+                        const direction = chooseDirection(i)
+                        return <ProjectMainCard key={data.title}  data={data} direction={direction} />
+                    })}
+            </ul>
+          </div>
+        {/* </Reveal> */}
+      </div>
+      <Footer />
+      <ScrollUp />
+    </>
+  );
 };
